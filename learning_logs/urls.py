9fba54524fa835da727689750b2a57210ccd9bd6
ShuangZeng <zengshuang.dl@gmail.com>
2021-03-07 23:50:43 -0600

    show page by passed parameter

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     #     主页
     #    Django在urlpatterns 中查找与请求的URL字符串匹配的正则表达式
     path('', views.index, name="index"),
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
